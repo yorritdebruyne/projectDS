dbf1296d94520ec3647614a36a948b0d42c700e0
Yorrit <yorritdebruyne@gmail.com>
2026-03-30 17:18:07 +0200

    Initial commit

--- src/main/java/org/example/lab3/Lab3Application.java
+++ src/main/java/org/example/lab3/Lab3Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Lab3Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Lab3Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Lab3Application.class, args);
+    }
 
 }
